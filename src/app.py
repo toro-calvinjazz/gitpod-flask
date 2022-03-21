c6e0409b5373619f104b15986ca9c65184c1628b
toro-calvinjazz <toro.calvinjazz.cruz@gmail.com>
2022-03-21 02:20:07 +0000

    update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-  return print('Hello, World!')
+  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
